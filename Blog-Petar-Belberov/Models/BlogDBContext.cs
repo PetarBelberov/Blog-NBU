5da0c853eef676b90e5d09e70c9539fe02168965
PetarBelberov <petar_belberov@gmx.com>
2017-01-04 17:17:32 +0200

    Added project changes

---
+++ Blog-Petar-Belberov/Models/BlogDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog_Petar_Belberov.Models
+{
+    public class BlogDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public BlogDBContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BlogDBContext Create()
+        {
+            return new BlogDBContext();
+        }
+    }
+}
---
+++ Blog-Petar-Belberov/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Blog_Petar_Belberov.Migrations;
+using Blog_Petar_Belberov.Models;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(Blog_Petar_Belberov.Startup))]
+namespace Blog_Petar_Belberov
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<BlogDBContext, Configuration>());
+             
+            ConfigureAuth(app);
+        }
+    }
+}
